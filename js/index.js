c2907d390858a1d97a23d368f39e3b90da297eec
Rubywong0321 <37624222+Rubywong0321@users.noreply.github.com>
2019-11-01 16:17:13 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-$(function(){
-    var $li = $('ul.tab-title li');
-        $($li. eq(0) .addClass('active').find('a').attr('href')).siblings('.tab-inner').hide();
-    
-        $li.click(function(){
-            $($(this).find('a'). attr ('href')).show().siblings ('.tab-inner').hide();
-            $(this).addClass('active'). siblings ('.active').removeClass('active');
-        });
-    });
+$(document).ready(function () {
+  $('.showMenu').on('click', function (event) {
+    event.preventDefault();
+    $('body').toggleClass('menu-show');
+  });
+  $(".menu li a").on("click", function (event) {
+    $('body').toggleClass('menu-show');
+  });
+});
